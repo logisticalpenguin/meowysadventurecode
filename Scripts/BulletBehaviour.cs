8d185cc36568f421d8ad321a68c8c27d0884783b
Mio <53755879+logisticalpenguin@users.noreply.github.com>
2022-05-31 22:46:48 +0800

    all scripts used in the game

---
+++ Scripts/BulletBehaviour.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletBehaviour : MonoBehaviour
+{
+    public float speed = 20;
+
+    // Start is called before the first frame update
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.right * speed * Time.deltaTime);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collisionGameObject)
+    {   
+
+            if (collisionGameObject.gameObject.tag.Equals("Enemy"))
+            {
+                EnemyBehaviour1 enemyHealth = collisionGameObject.gameObject.GetComponent<EnemyBehaviour1>();
+                enemyHealth.DinoGreenHit();
+                Destroy(gameObject);
+            }
+            else if (collisionGameObject.gameObject.tag.Equals("Destructable"))
+            {
+                Destroy(collisionGameObject.gameObject);
+                Destroy(gameObject);
+            }
+            else if (collisionGameObject.gameObject.tag.Equals("jumpPower") || collisionGameObject.gameObject.tag.Equals("speedPower") || collisionGameObject.gameObject.tag.Equals("speedjumpPower"))
+            {
+                // do nothing
+            }
+    }
+
+   }
